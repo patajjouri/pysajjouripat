04e6657284115a5b8bbac2e59641fec27c063ff1
patajjouri <pyspatajjouri@gmail.com>
2026-03-02 17:43:44 -0300

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      fontFamily: "Arial", 
+      textAlign: "center", 
+      marginTop: "100px" 
+    }}>
+      <h1>Welcome to Pysajjouripat 🌿</h1>
+      <p>This website is now live.</p>
+    </div>
+  );
+}

aec6eecba42c8e78a13e4d3f3ed49f1b7c8b56b3
patajjouri <pyspatajjouri@gmail.com>
2026-03-02 17:58:50 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      textAlign: "center",
+      marginTop: "100px"
+    }}>
+      <h1>Welcome to Pysajjouripat 🌿</h1>
+      <p>Your website is live.</p>
+    </div>
+  );
+}
